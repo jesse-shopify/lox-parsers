b3622e83ff8d76cc807ce41c211690febcbd15b8
Jesse Johnson <jesse.johnson@shopify.com>
2025-09-24 09:42:35 -0700

    Upgrade to nom 8

--- nom-lox/src/parser.rs
+++ nom-lox/src/parser.rs
@@ -1,24 +1,23 @@
-//! Nom-based parser for the Lox language
+//! Nom-based parser for the Lox language (nom 8.0)
 
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_while1, take_until},
+    bytes::complete::{tag, take_until, take_while1},
     character::complete::{
         alpha1, alphanumeric1, char, multispace0, multispace1,
     },
     combinator::{map, opt, recognize, value},
     multi::many0,
     number::complete::double,
-    sequence::{delimited, pair, preceded, terminated, tuple},
-    IResult
+    sequence::{delimited, pair, preceded, terminated},
+    IResult, Parser,
 };
 
 use lox_ast::{BinaryOp, Expr, Program, Stmt, UnaryOp, Value};
 
-
 /// Parse a line comment
 fn line_comment(input: &str) -> IResult<&str, ()> {
-    value((), pair(tag("//"), take_until("\n")))(input)
+    value((), pair(tag("//"), take_until("\n"))).parse(input)
 }
 
 /// Parse whitespace including comments
@@ -26,7 +25,7 @@ fn whitespace(input: &str) -> IResult<&str, ()> {
     value(
         (),
         many0(alt((value((), multispace1), line_comment))),
-    )(input)
+    ).parse(input)
 }
 
 /// Parse an identifier
@@ -37,7 +36,7 @@ fn identifier(input: &str) -> IResult<&str, String> {
             many0(alt((alphanumeric1, tag("_")))),
         )),
         |s: &str| s.to_string(),
-    )(input)
+    ).parse(input)
 }
 
 /// Parse a string literal
@@ -49,12 +48,12 @@ fn string_literal(input: &str) -> IResult<&str, String> {
             |s: &str| s.to_string(),
         ),
         char('"'),
-    )(input)
+    ).parse(input)
 }
 
 /// Parse a number literal
 fn number_literal(input: &str) -> IResult<&str, f64> {
-    double(input)
+    double.parse(input)
 }
 
 /// Parse a boolean literal
@@ -62,12 +61,12 @@ fn boolean_literal(input: &str) -> IResult<&str, bool> {
     alt((
         value(true, tag("true")),
         value(false, tag("false")),
-    ))(input)
+    )).parse(input)
 }
 
 /// Parse nil literal
 fn nil_literal(input: &str) -> IResult<&str, ()> {
-    value((), tag("nil"))(input)
+    value((), tag("nil")).parse(input)
 }
 
 /// Parse a literal value
@@ -77,7 +76,7 @@ fn literal(input: &str) -> IResult<&str, Value> {
         map(boolean_literal, Value::Bool),
         map(number_literal, Value::Number),
         map(string_literal, Value::String),
-    ))(input)
+    )).parse(input)
 }
 
 /// Parse a primary expression (literals, identifiers, groupings)
@@ -90,7 +89,7 @@ fn primary(input: &str) -> IResult<&str, Expr> {
             map(expression, |e| Expr::Grouping(Box::new(e))),
             delimited(multispace0, char(')'), multispace0),
         ),
-    ))(input)
+    )).parse(input)
 }
 
 /// Parse unary expressions
@@ -114,7 +113,7 @@ fn unary(input: &str) -> IResult<&str, Expr> {
             },
         ),
         primary,
-    ))(input)
+    )).parse(input)
 }
 
 /// Parse multiplication and division
@@ -131,7 +130,7 @@ fn factor(input: &str) -> IResult<&str, Expr> {
             multispace0,
         ),
         unary,
-    ))(input)?;
+    )).parse(input)?;
 
     Ok((input, ops.into_iter().fold(init, |acc, (op, expr)| {
         Expr::Binary {
@@ -156,7 +155,7 @@ fn term(input: &str) -> IResult<&str, Expr> {
             multispace0,
         ),
         factor,
-    ))(input)?;
+    )).parse(input)?;
 
     Ok((input, ops.into_iter().fold(init, |acc, (op, expr)| {
         Expr::Binary {
@@ -183,7 +182,7 @@ fn comparison(input: &str) -> IResult<&str, Expr> {
             multispace0,
         ),
         term,
-    ))(input)?;
+    )).parse(input)?;
 
     Ok((input, ops.into_iter().fold(init, |acc, (op, expr)| {
         Expr::Binary {
@@ -208,7 +207,7 @@ fn equality(input: &str) -> IResult<&str, Expr> {
             multispace0,
         ),
         comparison,
-    ))(input)?;
+    )).parse(input)?;
 
     Ok((input, ops.into_iter().fold(init, |acc, (op, expr)| {
         Expr::Binary {
@@ -226,7 +225,7 @@ fn logical_and(input: &str) -> IResult<&str, Expr> {
     let (input, ops) = many0(pair(
         delimited(multispace0, tag("and"), multispace0),
         equality,
-    ))(input)?;
+    )).parse(input)?;
 
     Ok((input, ops.into_iter().fold(init, |acc, (_, expr)| {
         Expr::Binary {
@@ -244,7 +243,7 @@ fn logical_or(input: &str) -> IResult<&str, Expr> {
     let (input, ops) = many0(pair(
         delimited(multispace0, tag("or"), multispace0),
         logical_and,
-    ))(input)?;
+    )).parse(input)?;
 
     Ok((input, ops.into_iter().fold(init, |acc, (_, expr)| {
         Expr::Binary {
@@ -259,18 +258,18 @@ fn logical_or(input: &str) -> IResult<&str, Expr> {
 fn assignment(input: &str) -> IResult<&str, Expr> {
     alt((
         map(
-            tuple((
+            (
                 identifier,
                 delimited(multispace0, char('='), multispace0),
                 assignment,
-            )),
+            ),
             |(name, _, value)| Expr::Assignment {
                 name,
                 value: Box::new(value),
             },
         ),
         logical_or,
-    ))(input)
+    )).parse(input)
 }
 
 /// Parse a full expression
@@ -281,26 +280,26 @@ fn expression(input: &str) -> IResult<&str, Expr> {
 /// Parse a print statement
 fn print_stmt(input: &str) -> IResult<&str, Stmt> {
     map(
-        tuple((
+        (
             delimited(multispace0, tag("print"), multispace0),
             expression,
             delimited(multispace0, char(';'), multispace0),
-        )),
+        ),
         |(_, expr, _)| Stmt::Print(expr),
-    )(input)
+    ).parse(input)
 }
 
 /// Parse a variable declaration
 fn var_declaration(input: &str) -> IResult<&str, Stmt> {
     map(
-        tuple((
+        (
             delimited(multispace0, tag("var"), multispace0),
             delimited(multispace0, identifier, multispace0),
             opt(preceded(delimited(multispace0, char('='), multispace0), expression)),
             delimited(multispace0, char(';'), multispace0),
-        )),
+        ),
         |(_, name, initializer, _)| Stmt::VarDeclaration { name, initializer },
-    )(input)
+    ).parse(input)
 }
 
 /// Parse an expression statement
@@ -308,7 +307,7 @@ fn expr_stmt(input: &str) -> IResult<&str, Stmt> {
     map(
         terminated(expression, delimited(multispace0, char(';'), multispace0)),
         Stmt::Expression,
-    )(input)
+    ).parse(input)
 }
 
 /// Parse a statement
@@ -320,7 +319,7 @@ fn statement(input: &str) -> IResult<&str, Stmt> {
             var_declaration,
             expr_stmt,
         )),
-    )(input)
+    ).parse(input)
 }
 
 /// Parse a program (list of statements)
@@ -328,7 +327,7 @@ pub fn program(input: &str) -> IResult<&str, Program> {
     map(
         terminated(many0(statement), whitespace),
         Program::new,
-    )(input)
+    ).parse(input)
 }
 
 /// Parse a complete Lox program from a string
